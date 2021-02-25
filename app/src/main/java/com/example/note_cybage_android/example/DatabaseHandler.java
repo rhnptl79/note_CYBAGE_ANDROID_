6891787abdd78638c561fbfb664b831a8633f63a
Rohan <rhnptl79@gmail.ccom>
2021-02-25 17:34:26 +0530

    Mohit Note 31

---
+++ app/src/main/java/com/example/note_cybage_android/example/DatabaseHandler.java
@@ -0,0 +1,9 @@
+package com.example.note_cybage_android.example;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+
+
+}
---
+++ app/src/main/java/com/example/note_cybage_android/example/LocationTrack.java
@@ -0,0 +1,5 @@
+package com.example.note_cybage_android.example;
+
+public class LocationTrack {
+
+}
