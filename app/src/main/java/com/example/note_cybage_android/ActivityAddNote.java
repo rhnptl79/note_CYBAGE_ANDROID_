d911961e2b8a8947547952ad0bdf44567ac90df1
Rohan <rhnptl79@gmail.ccom>
2021-02-25 17:10:03 +0530

    Sanjay Note 26

--- app/src/main/java/com/example/note_cybage_android/ActivityAddNote.java
+++ app/src/main/java/com/example/note_cybage_android/ActivityAddNote.java
@@ -288,19 +288,25 @@ public class ActivityAddNote extends AppCompatActivity {
             choosePhotoHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
 
+    }
 
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
+    }
 
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
 
+    }
 
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
 
-
-
-
-
-
-
+    }
 
 }
 
