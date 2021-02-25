bad122a56a192de47dfbc901dcca88921f259ccf
Rohan <rhnptl79@gmail.ccom>
2021-02-25 21:49:15 +0530

    Amandeep Note 48

--- app/src/main/java/com/example/note_cybage_android/example/AdapterNotes.java
+++ app/src/main/java/com/example/note_cybage_android/example/AdapterNotes.java
@@ -1,7 +1,12 @@
 package com.example.note_cybage_android.example;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -17,6 +22,30 @@ public class AdapterNotes extends RecyclerView.Adapter<AdapterNotes.MyViewHolder
         this._list = _list;
         this._db = db;
     }
+
+    @NonNull
+    @Override
+    public AdapterNotes.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.adapter_notes_list, parent, false);
+
+        return new AdapterNotes.MyViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterNotes.MyViewHolder holder, int position) {
+        holder.tv_name.setText(_list.get(position).getTitle());
+        holder.tv_date.setText(_list.get(position).getTimeStamp());
+
+        holder.rl_notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(_context, ActivityAddNote.class);
+                intent.putExtra("edit", _list.get(position));
+                intent.putExtra("catId", String.valueOf(_list.get(position).getCatId()));
+                _context.startActivity(intent);
+            }
+        });
 
 
 }
