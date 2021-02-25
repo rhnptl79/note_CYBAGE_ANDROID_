9330aea8a5119ba4c61827bc43f9d4f456515601
Rohan <rhnptl79@gmail.ccom>
2021-02-25 16:38:55 +0530

    Rohan Note 9

--- app/src/main/java/com/example/note_cybage_android/ActivityNotesList.java
+++ app/src/main/java/com/example/note_cybage_android/ActivityNotesList.java
@@ -3,11 +3,15 @@ package com.example.note_cybage_android;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ActivityNotesList extends AppCompatActivity {
 
@@ -31,6 +35,30 @@ public class ActivityNotesList extends AppCompatActivity {
         db=new DatabaseHandler(this);
         initView();
 
+        btn_add_notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ActivityNotesList.this, ActivityAddNote.class);
+                intent.putExtra("catId",getIntent().getStringExtra("catId"));
+                startActivity(intent);
+            }
+        });
 
+        btn_sort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                listData.sort(String::compareToIgnoreCase);
+                Collections.sort(listData, new Comparator<NotesData>() {
+                    @Override
+                    public int compare(NotesData item, NotesData t1) {
+                        String s1 = item.getTitle();
+                        String s2 = t1.getTitle();
+                        return s1.compareToIgnoreCase(s2);
+                    }
+
+                });
+                adapterNotes.notifyDataSetChanged();
+            }
+        });
     }
 }
