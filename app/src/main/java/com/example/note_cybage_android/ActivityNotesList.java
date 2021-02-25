88a135ecb5e634cb35310518fc6e2ba7142070aa
Rohan <rhnptl79@gmail.ccom>
2021-02-25 16:37:28 +0530

    Rohan Note 8

--- app/src/main/java/com/example/note_cybage_android/ActivityNotesList.java
+++ app/src/main/java/com/example/note_cybage_android/ActivityNotesList.java
@@ -1,14 +1,36 @@
 package com.example.note_cybage_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class ActivityNotesList extends AppCompatActivity {
+
+    RecyclerView rv_notes_list;
+    DatabaseHandler db;
+    ArrayList<NotesData> listData=new ArrayList<>();
+    ImageView btn_add_notes,btn_sort;
+    AdapterNotes adapterNotes;
+    EditText et_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+
+        rv_notes_list=findViewById(R.id.rv_notes_list);
+        btn_add_notes=findViewById(R.id.btn_add_notes);
+        btn_sort=findViewById(R.id.btn_sort);
+        et_search=findViewById(R.id.et_search);
+
+        db=new DatabaseHandler(this);
+        initView();
+
+
     }
 }
