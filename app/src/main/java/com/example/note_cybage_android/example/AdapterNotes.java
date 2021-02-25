3caeb9859533a6af4f923eefa146249355734ed3
Rohan <rhnptl79@gmail.ccom>
2021-02-25 21:48:33 +0530

    Amandeep Note 47

--- app/src/main/java/com/example/note_cybage_android/example/AdapterNotes.java
+++ app/src/main/java/com/example/note_cybage_android/example/AdapterNotes.java
@@ -1,10 +1,22 @@
 package com.example.note_cybage_android.example;
 
+import android.content.Context;
+
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class AdapterNotes extends RecyclerView.Adapter<AdapterNotes.MyViewHolder> {
 
+    Context _context;
+    ArrayList<NotesData> _list;
+    DatabaseHandler _db;
 
+    public AdapterNotes(Context _context, ArrayList<NotesData> _list, DatabaseHandler db) {
+        this._context = _context;
+        this._list = _list;
+        this._db = db;
+    }
 
 
 }
