dae14a5ac59067292a7622518cf7147a3cbf9523
Rohan <rhnptl79@gmail.ccom>
2021-02-25 21:48:05 +0530

    Amandeep Note 46

---
+++ app/src/main/java/com/example/note_cybage_android/example/AdapterNotes.java
@@ -0,0 +1,10 @@
+package com.example.note_cybage_android.example;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterNotes extends RecyclerView.Adapter<AdapterNotes.MyViewHolder> {
+
+
+
+
+}
