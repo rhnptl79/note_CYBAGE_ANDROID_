43cf18d4620cc3401bcc1676cbf1f086e15c851f
Rohan <rhnptl79@gmail.ccom>
2021-02-25 17:48:58 +0530

    Mohit Note 44

---
+++ app/src/main/java/com/example/note_cybage_android/example/CategoryData.java
@@ -0,0 +1,12 @@
+package com.example.note_cybage_android.example;
+
+public class CategoryData {
+    public CategoryData(int id,String name){
+        this.cId=id;
+        this.cName=name;
+    }
+
+    String cName;
+    int cId;
+
+}
