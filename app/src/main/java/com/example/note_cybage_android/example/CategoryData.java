ae0ea3fe25be9faca5002b71e32dd7faa8e496b9
Rohan <rhnptl79@gmail.ccom>
2021-02-25 17:49:19 +0530

    Mohit Note 45

--- app/src/main/java/com/example/note_cybage_android/example/CategoryData.java
+++ app/src/main/java/com/example/note_cybage_android/example/CategoryData.java
@@ -8,5 +8,19 @@ public class CategoryData {
 
     String cName;
     int cId;
+    public String getcName() {
+        return cName;
+    }
 
+    public void setcName(String cName) {
+        this.cName = cName;
+    }
+
+    public int getcId() {
+        return cId;
+    }
+
+    public void setcId(int cId) {
+        this.cId = cId;
+    }
 }
