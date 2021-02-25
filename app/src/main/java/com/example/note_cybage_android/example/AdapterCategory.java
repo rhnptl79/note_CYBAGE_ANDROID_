974aab699a8bc2c39e545d79c4111c5f1051286d
Rohan <rhnptl79@gmail.ccom>
2021-02-25 21:55:38 +0530

    Amandeep Note 54

--- app/src/main/java/com/example/note_cybage_android/example/AdapterCategory.java
+++ app/src/main/java/com/example/note_cybage_android/example/AdapterCategory.java
@@ -1,7 +1,14 @@
 package com.example.note_cybage_android.example;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -23,7 +30,46 @@ public class AdapterCategory extends RecyclerView.Adapter<AdapterCategory.MyView
         this.listener=listener;
     }
 
+    @NonNull
+    @Override
+    public AdapterCategory.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.adapter_category, parent, false);
 
+        return new MyViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterCategory.MyViewHolder holder, int position) {
+
+        if (_status){
+            if (_notesData.getCatId()!=_list.get(position).getcId()){
+                holder.tv_name.setText(_list.get(position).getcName());
+            }
+        }else{
+            holder.tv_name.setText(_list.get(position).getcName());
+        }
+        holder.rm_cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _db.deleteCategory(new CategoryData(_list.get(position).getcId(),_list.get(position).getcName()),listener);
+            }
+        });
+        holder.rv_base_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (_status){
+                    _db.moveNotes(_notesData,String.valueOf(_list.get(position).getcId()));
+                    Toast.makeText(_context,"Moved!!",Toast.LENGTH_SHORT).show();
+                    ((Activity)_context).finish();
+                }else {
+                    Intent intent = new Intent(_context, ActivityNotesList.class);
+                    intent.putExtra("catId", String.valueOf(_list.get(position).getcId()));
+                    _context.startActivity(intent);
+
+                }}
+        });
+    }
 
 
 }
